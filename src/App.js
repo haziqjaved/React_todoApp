76ea32e8af73fc9cfc815d528cc399dea122fa1c
haziqjaved <haziqjaved31@gmail.com>
2021-09-23 00:24:09 +0500

    'TodoApp'

--- src/App.js
+++ src/App.js
@@ -2,43 +2,72 @@ import React from "react";
 import { useState } from 'react' //hooks (built in functions)
 import DeleteIcon from '@material-ui/icons/DeleteOutlined';
 import AddIcon from "@material-ui/icons/Add";
+import DeleteForeverIcon from '@material-ui/icons/DeleteForeverOutlined';
 import { EditOutlined } from "@material-ui/icons";
 import './App.css';
 
-// TODO Applications
+  // TODO Applications
 function App() {
   const [todoList, setTodoList] = useState([])  //for adding a data into list we array
-  const [text, setText] = useState('')
+  const [text, setText] = useState('')  //for inp
+  const [toogleBtn,setToogleBtn]=useState(false) //for playing with toogle
+  const [currentEditIndex, setCurrentEditIndex] = useState(-1)
 
   // FOR ADD Data
-  const addItem =_=>{
+  const addItem =() =>{
         const listApp = [...todoList] //for changing reference we use spread operator
         listApp.push(text)
-        setTodoList(listApp)}
-  // Delete 
-    const deleteList=(value,index)=>{
+        setTodoList(listApp)
+        setText('')
+      
+      }
+    // Delete 
+    const deleteList=(val,index)=>{
       const listApp = [...todoList] 
       listApp.splice(index,1)
       setTodoList(listApp)
     }
-  return (
+    const DeleteAll=()=>{
+      setTodoList([])
+    }
     
+    const editItem=(index)=>{
+      setText(todoList[index]);
+      setToogleBtn(true)
+    }
+    const updateItem = () =>{
+      const listApp = [...todoList];
+      listApp.splice(currentEditIndex,1,text);
+      setTodoList(listApp);
+      setToogleBtn(false);
+      setCurrentEditIndex(-1);
+      setText('')
+    }
+
+  return (
       <div className="centered" align="center" >
         <br />
         <h1 >Todo List</h1>
         <br />
-        <input  onChange={(e)=> setText(e.target.value) } type='text' placeholder='Add Items' />
-        <button className='add' onClick={addItem} ><AddIcon /></button>
+        <input  onChange={(e)=> setText(e.target.value)  }  value={text} type='text' placeholder='Add Items' />
+        {
+          toogleBtn?<button className='edit' onClick={updateItem} ><EditOutlined/></button>:
+          <button className='add' onClick={addItem} ><AddIcon /></button>
+        }
+        
         <ul>
         {
-          todoList.map((item,index)=>{
-           return <li>{item}
-          <button className='edit' ><EditOutlined/></button>
+          todoList.map((item,index)=>
+          {
+           return <li key={index}>
+           {item}
+          <button className='edit' onClick={()=>editItem(index)} ><EditOutlined/></button>
           <button className='delete' onClick={()=>deleteList(index)}> <DeleteIcon /> </button>
            </li>
           })
         }
         </ul>
+        <div className='deleteAll' onClick={DeleteAll} align='center'><DeleteForeverIcon/></div>
       </div>
 
   );
